2e927f0355514442f981fa1bed37fa8b3062f05c
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-07 12:26:50 +0300

    C3 1

--- src/by/it/popkov/jd01_02/TaskC.java
+++ src/by/it/popkov/jd01_02/TaskC.java
@@ -7,8 +7,47 @@ public class TaskC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[][] a = step1(n);
-        step2(a);
+        int[][] array = step1(n);
+        step2(array);
+        step3(array);
+    }
+
+    private static int[][] step3(int[][] mas) {
+        int[][] outPut = new int[mas.length][mas.length];
+        int max = mas[0][0];
+        int posI = 0;
+        int posJ = 0;
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas.length; j++) {
+                if (mas[i][j] > max) {
+                    max = mas[i][j];
+                    posI = i;
+                    posJ = j;
+                }
+            }
+        }
+        for (int i = 0; i < outPut.length; i++) {
+            if (i == posI) {
+                continue;
+            }
+            for (int j = 0; j < outPut.length; j++) {
+                if (j == posJ) {
+                    continue;
+                }
+                outPut[i][j] = mas[i][j];
+                System.out.print(outPut[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        for (int i = 0; i < outPut.length; i++) {
+            for (int j = 0; j < outPut.length; j++) {
+                if (outPut[i][j] == max) {
+                    outPut = step3(outPut);
+                }
+            }
+        }
+
+        return outPut;
     }
 
     private static int[][] step1(int n) {
