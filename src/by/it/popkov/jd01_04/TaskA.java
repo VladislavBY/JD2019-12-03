0ec3ae95c8b961f6b0ce28dbbea9ad1face8a0a4
Vladislav Popkov <vladislav.minsk.by@gmail.com>
2019-12-12 12:23:54 +0300

    A2 is done

--- src/by/it/popkov/jd01_04/TaskA.java
+++ src/by/it/popkov/jd01_04/TaskA.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class TaskA {
     public static void main(String[] args) {
         printMulTable();
-        buildOneDimArray("4 5 7 4 3 5 5 9 8 4 ");
+        buildOneDimArray("4 5 7 4 3 5 5 9 8");
     }
 
     static void buildOneDimArray(String line) {
         double[] array = InOut.getArray(line);
-        InOut.printArray(array ,"V", 5);
+        InOut.printArray(array, "V", 5);
         double first = array[0];
-        double last = array[array.length];
+        double last = array[array.length - 1];
         Helper.sort(array);
         InOut.printArray(array, "V", 4);
         System.out.printf("Index of first element=%d\n", Arrays.binarySearch(array, first));
@@ -20,10 +20,10 @@ public class TaskA {
 
     }
 
-    static  void  printMulTable(){
+    static void printMulTable() {
         for (int i = 0; i <= 9; i++) {
             for (int j = 0; j <= 9; j++) {
-                System.out.printf("%1d*%1d=%-3d", i, j , i*j);
+                System.out.printf("%1d*%1d=%-3d", i, j, i * j);
             }
             System.out.println();
         }
